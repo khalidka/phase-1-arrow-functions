92067f842e21bc90d622c6e7fcdc43d404cab3e0
root <root@DESKTOP-3S5SIJL.localdomain>
2023-12-12 20:13:21 +0300

    completed  assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = () => 2000 / 100;
+console.log(divide());
+
+const square = () => 2 ** 2;
+console.log(square());
+
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(3, 4));
